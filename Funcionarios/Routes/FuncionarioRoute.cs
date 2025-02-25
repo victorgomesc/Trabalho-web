db354513ba3026ca28b562378a97fccfd1bc5474
victor gomes <victorgomesdacosta18@gmail.com>
2025-02-24 22:39:25 -0300

    feat:  create buscar

--- Funcionarios/Routes/FuncionarioRoute.cs
+++ Funcionarios/Routes/FuncionarioRoute.cs
@@ -11,17 +11,45 @@ public static class TaskRoute {
 
         route.MapPost("", async (FuncionarioRequest req, FuncionarioContext context ) => 
         {
-        var funcionario = new FuncionarioModel(req.name, req.cargo, req.cpf, req.salario);
-        await context.AddAsync(funcionario);
-        await context.SaveChangesAsync();
-    
-         return Results.Ok(funcionario);
+            var funcionario = new FuncionarioModel(req.name, req.cargo, req.cpf, req.salario);
+            await context.AddAsync(funcionario);
+            await context.SaveChangesAsync();
+        
+            return Results.Ok(funcionario);
         });
 
         route.MapGet("", async (FuncionarioContext context) => 
         {
             var funcionario = await context.Funcionario.ToListAsync();
             return Results.Ok(funcionario);
+        });
+
+        route.MapGet("/buscar/{name}", async (string name, FuncionarioContext context) => 
+        {
+            var funcionario = await context.Funcionario.FirstOrDefaultAsync(x => x.Name == name);
+            if (funcionario == null)
+            {
+                return Results.NotFound();
+            }
+            return Results.Ok(funcionario);
+        });
+
+        route.MapGet("/media-salarial/{cargo}", async (string cargo, FuncionarioContext context) => 
+        {
+            var mediaSalarial = await context.Funcionario
+                .Where(f => f.Cargo == cargo)
+                .AverageAsync(f => f.Salario);
+
+            return Results.Ok(new { Cargo = cargo, MediaSalarial = mediaSalarial });
+        });
+
+        route.MapGet("/funcionarios/{cargo}", async (string cargo, FuncionarioContext context) => 
+        {
+            var funcionarios = await context.Funcionario
+                .Where(f => f.Cargo == cargo)
+                .ToListAsync();
+
+            return Results.Ok(funcionarios);
         });
 
         route.MapPut("{id:guid}", async (Guid id, FuncionarioRequest req, FuncionarioContext context) => 
@@ -55,12 +83,10 @@ public static class TaskRoute {
                 return Results.NotFound();
             }
 
-
             context.Funcionario.Remove(funcionario);
             await context.SaveChangesAsync();
 
             return Results.NoContent();
         });
-
     }
 }
